28336916bade1412896ecaeed27e2461f398b5b0
Neeraj Rana <neeraj@getcovered.io>
2024-08-10 02:24:30 +0530

    added form,states and reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './styles.css'
+import {useReducer} from "react";
+import TicketForm from "./components/TicketForm";
+import ticketReducer from "./reducers/ticketReducer";
 function App() {
+
+  const initialState = {tickets: []}
+  const [state, dispatch] = useReducer(ticketReducer, initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="container">
+    <h1>Bug Blaster</h1>
+    <TicketForm dispatch={dispatch}></TicketForm>
+  </div>
     </div>
   );
 }
---
+++ src/components/TicketForm.js
@@ -0,0 +1,63 @@
+import React, {useState} from "react";
+
+export default function TicketForm({dispatch}){
+    const [title,setTitle] = useState("");
+    const [description,setDescription] = useState("");
+    const [priority,setPriority] = useState("1");
+
+    const priorityLabels = {
+        1: "Low",
+        2: 'Medium',
+        3: 'High'
+    }
+    const clearForm = () =>{
+        setTitle("")
+        setDescription("")
+        setPriority("1")
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        const ticketData = {
+            id: new Date().toISOString(),
+            title,
+            description,
+            priority
+        }
+
+        dispatch({
+            type: "ADD_TICKET",
+            payload: ticketData
+        });
+
+        clearForm();
+
+
+    }
+    return(
+        <form onSubmit={handleSubmit} className="ticket-form">
+            <div>
+                <label>Title</label>
+                <input type="text" value={title} className="form-input" onChange={e => setTitle(e.target.value)} />
+            </div>
+            <div>
+                <label>Description</label>
+                <textarea type="text" value={description} className="form-input" onChange={e => setDescription(e.target.value)} />
+            </div>
+            <fieldset className="priority-fieldset">
+                <legend>Priority</legend>
+                {
+                    Object.entries(priorityLabels).map(([value,label]) => (
+                        <label key={value} className="priority-label">
+
+                            <input className="priority-input" type="radio" name="priority" value={value} checked={priority === value} onChange={(e) => setPriority(e.target.value)}/>
+                            {label}
+                        </label>
+                    ))
+                }
+            </fieldset>
+            <button type="submit">Submit</button>
+        </form>
+
+    )
+}
